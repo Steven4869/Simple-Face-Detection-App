84c02f67841594e321f85659af6613826fe42c90
Shivam Sinha <78679002+Steven4869@users.noreply.github.com>
2021-12-06 13:29:31 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,52 @@
+let video = document.getElementById("video");
+let model;
+
+// declare the canvas variable and setting up the context 
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+const accessCamera = () => {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: { width: 500, height: 400 },
+      audio: false,
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+    });
+};
+
+const detectFaces = async () => {
+  const prediction = await model.estimateFaces(video, false);
+
+  // Using canvas to draw the video first
+
+  ctx.drawImage(video, 0, 0, 500, 400);
+
+  prediction.forEach((predictions) => {
+    
+    // Drawing rectangle that'll detect the face
+    ctx.beginPath();
+    ctx.lineWidth = "4";
+    ctx.strokeStyle = "yellow";
+    ctx.rect(
+      predictions.topLeft[0],
+      predictions.topLeft[1],
+      predictions.bottomRight[0] - predictions.topLeft[0],
+      predictions.bottomRight[1] - predictions.topLeft[1]
+    );
+    ctx.stroke();
+  });
+};
+
+accessCamera();
+video.addEventListener("loadeddata", async () => {
+  model = await blazeface.load();
+  // Calling the detectFaces 40 times per second
+  setInterval(detectFaces, 40);
+});
+
+// the last two arguments are width and height
+    // since blazeface returned only the coordinates, 
+    // we can find the width and height by subtracting them.
